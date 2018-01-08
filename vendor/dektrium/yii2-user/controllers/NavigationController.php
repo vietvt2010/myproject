83544063a9b2d9add8abaa6efa3f0efb8b07dfd9
vietvt <vietvotrung@admicro.vn>
2018-01-08 17:28:50 +0700

    8.1

--- vendor/dektrium/yii2-user/controllers/NavigationController.php
+++ vendor/dektrium/yii2-user/controllers/NavigationController.php
@@ -4,18 +4,58 @@ namespace dektrium\user\controllers;
 
 use app\models\Navigation;
 use yii\data\ActiveDataProvider;
+use yii\web\Response;
+use yii\helpers\Html;
 
 class NavigationController extends \yii\web\Controller
 {
+    /**
+     * action index
+     * @return type
+     */
     public function actionIndex()
     {
         $dataProvider = new ActiveDataProvider([
             'query' => Navigation::find(),
+            'pagination' => [
+                'pageSize' => 20,
+            ],
         ]);
+        
+        if (\Yii::$app->request->post('hasEditable')) {
+            return $this->_handleEditable();
+        }
         
         return $this->render('index', [
             'dataProvider' => $dataProvider,
         ]);
     }
 
+    /**
+     * handle editable grid column
+     */
+    public function _handleEditable()
+    {
+        \Yii::$app->response->format = Response::FORMAT_JSON;
+        
+        $id = \Yii::$app->request->post('editableKey');
+        $index = \Yii::$app->request->post('editableIndex');
+        $attribute = \Yii::$app->request->post('editableAttribute');
+        
+        $model = Navigation::findOne($id);
+        
+        if ($model && $model->hasAttribute($attribute)) {
+            $attributeValue = \yii\helpers\ArrayHelper::getValue(\Yii::$app->request->post(), "Navigation.$index.$attribute");
+            
+            $model->setAttribute($attribute, $attributeValue);
+            
+            if ($model->save(true, [$attribute])) {
+                return ['output' => Html::encode($model->$attribute), 'message' => ''];
+            } else {
+                return ['output' => Html::encode($model->$attribute), 'message' => \yii\helpers\ArrayHelper::getValue($model->getErrors($attribute), '0')];
+            }
+        }
+        
+        return ['output' => '', 'message' => ''];
+    }
 }
--- vendor/dektrium/yii2-user/views/navigation/index.php
+++ vendor/dektrium/yii2-user/views/navigation/index.php
@@ -3,26 +3,41 @@
 
 use kartik\editable\Editable;
 use kartik\grid\GridView;
+use yii\data\ActiveDataProvider;
+
 ?>
-<?php
-GridView::widget([
-    'dataProvider' => $dataProvider,
-    'columns' => [
-        [
-            'class' => 'yii\grid\SerialColumn',
-            'options' => ['width' => '10px'],
-        ],
-        [
-            'attribute' => 'name',
-            'class' => 'kartik\grid\EditableColumn',
-            'editableOptions' => function ($model, $key, $index) {
-                return [
-                    'readonly' => false,
-                    'inputType' => Editable::INPUT_TEXT,
-                    'options' => []
-                ];
-            },
-        ],
-    ],
-]);
-?>
+
+<div class="row" style="margin: 5px">
+    <div class="col-md-12">
+        <div class="box box-primary">
+            <div class="box-header with-border">
+                <h4 style="display: table;" class="pull-left">Quản lý danh mục</h4>
+            </div>
+            
+            <div class="box-body">
+                <?=
+                GridView::widget([
+                    'dataProvider' => $dataProvider,
+                    'columns' => [
+                        [
+                            'class' => 'yii\grid\SerialColumn',
+                            'options' => ['width' => '10px'],
+                        ],
+                        [
+                            'attribute' => 'name',
+                            'class' => 'kartik\grid\EditableColumn',
+                            'editableOptions' => function ($model, $key, $index) {
+                                return [
+                                    'readonly' => false,
+                                    'inputType' => Editable::INPUT_TEXT,
+                                    'options' => []
+                                ];
+                            },
+                        ],
+                    ],
+                ]);
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
